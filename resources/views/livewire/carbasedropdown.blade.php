1e6edf834e86dd900c333ce89c59d7206e4cd225
Rafal Makowski <rafael.makowski95@gmail.com>
2022-12-07 20:00:52 +0100

    Zmiana animacji ładowania w bazie aut na koła zębate

--- resources/views/livewire/carbasedropdown.blade.php
+++ resources/views/livewire/carbasedropdown.blade.php
@@ -5,12 +5,7 @@
     <div wire:loading>
         <div style="display: flex; justify-content: center; align-items: center; background-color: black;
         position: fixed; top: 0px; left: 0px; z-index: 9999; width: 100%; height: 100%; opacity: .75;">
-            <div class="la-ball-spin-clockwise la-2x">
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div class="la-cog la-3x">
                 <div></div>
                 <div></div>
                 <div></div>
