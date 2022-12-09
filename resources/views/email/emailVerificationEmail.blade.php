de41e0a6ca838c0d4f2be27f10c654299a5f8ff6
darkac5 <dariuszkaczmarek1@gmail.com>
2022-12-09 19:24:27 +0100

    mail ver

--- resources/views/email/emailVerificationEmail.blade.php
+++ resources/views/email/emailVerificationEmail.blade.php
@@ -1,29 +1,68 @@
  <!-- Fonts -->
- <head>
- <link href="/css/main.css" rel="stylesheet">
-    <link href="/css/bootstrap.css" rel="stylesheet">
-    <link href="/css/all.css" rel="stylesheet">
-    <!-- icons -->
-    <script src="https://kit.fontawesome.com/87cc08ad60.js" crossorigin="anonymous"></script>
-    <!--favicon-->
-    <link rel="icon" href="favicon.ico" type="image/vnd.microsoft.icon">
++<head>
+ <style>
+.angry-grid {
+   width: 30%;
+   margin-left:auto;
+   margin-right: auto;
+   color: #ffffff;
+}
+  
+#item-0 {
+   margin:20px;
+}
+#item-1 {
+   text-align: center; 
+}
+#item-2 { 
+margin-top:30px;
+}
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.button {
+  text-decoration: none;
+  color: #000000;
+  background: #ffc107;
+  padding: 15px 40px;
+  border-radius: 4px;
+  font-weight: normal;
+  text-transform: uppercase;
+  transition: all 0.2s ease-in-out;
+  margin-left:auto;
+   margin-right: auto;
+   margin-top:20px;
+}
 
-    <link href="css/main.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/all.css" rel="stylesheet">
-</head>
-<body style="background-color:#252525">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6 mt-5 text-center text-white ">
-                 <a class="navbar-brand" style:"margin-left:35%" href="/"><img src="{{ URL::to('/img/icon/logo.png') }}" width="250" alt=""></a>
-                 <h2 class="m-5">Zweryfikuj swój adres e-mail</h2>
+.glow-button:hover {
+  color: rgba(255, 255, 255, 1);
+  box-shadow: 0 5px 15px rgba(145, 92, 182, .4);
+}
+.center-btn {
+  margin: 0;
+  position: absolute;
+
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+
+</style>
+
+    <div class="angry-grid">
+		  <div id="item-0">
+			<a><img src="https://bi.im-g.pl/im/31/f4/18/z26165809IER,Izera.jpg" class="center" width="250" alt=""></a>
+		  </div>
+		<div id="item-1">
+			   <h2 class="m-5">Zweryfikuj swój adres e-mail</h2>
                     Dziękujemy na rejestrację na naszej stronie. Aby zakończyć proces rejestracji aktywuj swoje konto po przed weryfikację adresu e-mail
                     By zweryfikować email kliknij w link poniżej:  </br></br>
-
-                    <a class="btn btn-warning d-flex justify-content-center font-weight-bold" style="width: 80%; margin-left: auto; margin-right: auto;" style="width: 50%" href="{{ route('user.verify', $token) }}" role="button">Weryfikuj e-mail</a>   
-                
-            </div>
-        </div>
-    </div>        
+                    </div>
+		<div id="item-2">
+			<a href="{{ route('user.verify', $token) }}" class="button glow-button center-btn" target="_blank" role="button">Zweryfikuj E-mail</a>
+		</div>
+	</div>
+               
 </body>
